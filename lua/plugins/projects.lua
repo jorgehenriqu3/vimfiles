7c27e429aac61d48e7ad208ed304f935611e7f40
Jorge Henrique <jorge.henrique@nubank.com.br>
2026-01-26 19:19:38 -0300

    add coffebar/neovim-project

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -1,38 +1,32 @@
 return {
   {
-    "ahmedkhalf/project.nvim",
-    config = function()
-      require("project_nvim").setup({
-        detection_methods = { "pattern", "lsp" },
-        patterns = { ".git", "project.clj", "package.json" },
-        silent_chdir = true,
-      })
-    end,
+    "Shatur/neovim-session-manager",
+    lazy = false,
+    priority = 101,
   },
   {
-    "nvim-telescope/telescope-project.nvim",
+    "coffebar/neovim-project",
     dependencies = {
+      "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim",
+      "Shatur/neovim-session-manager",
     },
-    config = function()
-      require("telescope").setup({
-        extensions = {
-          project = {
-            base_dirs = {
-              { path = "~/dev/nu", max_depth = 4 },
-            },
-            hidden_files = false,
-            order_by = "recent",
-            search_by = "title",
-            on_project_selected = function(prompt_bufnr)
-              local project_actions = require("telescope._extensions.project.actions")
-              project_actions.change_working_directory(prompt_bufnr, false)
-            end,
-          },
-        },
-      })
-
-      require("telescope").load_extension("project")
+    init = function()
+      vim.opt.sessionoptions:append("globals")
     end,
+    opts = {
+      projects = {
+        "~/dev/nu/*",
+        "~/dev/*",
+        "~/.config/*",
+      },
+    },
+    keys = {
+      { "<leader>pp", "<cmd>NeovimProjectDiscover<cr>", desc = "Projects" },
+      { "<leader>ph", "<cmd>NeovimProjectHistory<cr>", desc = "Project History" },
+      { "<leader>pr", "<cmd>NeovimProjectLoadRecent<cr>", desc = "Recent Project" },
+    },
+    lazy = false,
+    priority = 100,
   },
 }
