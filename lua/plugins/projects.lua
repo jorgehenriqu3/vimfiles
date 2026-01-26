6728e6e257f13cf7c4d20c7a4a91f16ed0ad0833
Jorge Henrique <jorge.henrique@nubank.com.br>
2026-01-26 20:06:23 -0300

    add intellij theme

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -29,4 +29,43 @@ return {
     lazy = false,
     priority = 100,
   },
+  {
+    "pmouraguedes/neodarcula.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require("neodarcula").setup({
+        transparent = false,
+        dim = false,
+      })
+      vim.cmd([[colorscheme neodarcula]])
+      
+      vim.api.nvim_set_hl(0, "Normal", { bg = "#2B2B2B" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#313335" })
+      vim.api.nvim_set_hl(0, "NormalNC", { bg = "#2B2B2B" })
+      
+      vim.api.nvim_set_hl(0, "CursorLine", { bg = "#323232" })
+      vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#A9B7C6", bg = "#323232", bold = true })
+      vim.api.nvim_set_hl(0, "Visual", { bg = "#214283" })
+      
+      vim.api.nvim_set_hl(0, "NeoTreeNormal", { bg = "#313335" })
+      vim.api.nvim_set_hl(0, "NeoTreeNormalNC", { bg = "#313335" })
+      vim.api.nvim_set_hl(0, "NeoTreeCursorLine", { bg = "#3E4042" })
+      
+      vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#3E4042", bg = "NONE" })
+      vim.api.nvim_set_hl(0, "VertSplit", { fg = "#3E4042", bg = "NONE" })
+      
+      vim.api.nvim_set_hl(0, "FloatBorder", { fg = "#CC7832", bg = "#313335" })
+      vim.api.nvim_set_hl(0, "FloatTitle", { fg = "#A9B7C6", bg = "#313335" })
+      
+      vim.api.nvim_set_hl(0, "WhichKeyFloat", { bg = "#313335" })
+      vim.api.nvim_set_hl(0, "WhichKeyBorder", { fg = "#CC7832", bg = "#313335" })
+      
+      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#CC7832", bg = "#313335" })
+      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "#313335" })
+      
+      vim.api.nvim_set_hl(0, "IncSearch", { fg = "#000000", bg = "#CC7832", bold = true })
+      vim.api.nvim_set_hl(0, "Search", { fg = "#000000", bg = "#CC7832" })
+    end,
+  },
 }
