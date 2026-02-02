562e5b7bd788fb2bce15d43a130da969eb161332
Jorge Henrique <jorge.henrique@nubank.com.br>
2026-02-02 15:00:30 -0300

    add autosave

--- lua/plugins/projects.lua
+++ lua/plugins/projects.lua
@@ -57,4 +57,16 @@ return {
       require("nordic").load()
     end,
   },
+  {
+    "okuuva/auto-save.nvim",
+    event = { "InsertLeave", "TextChanged" },
+    opts = {
+      enabled = true,
+      trigger_events = {
+        immediate_save = { "BufLeave", "FocusLost" },
+        defer_save = { "InsertLeave", "TextChanged" },
+      },
+      debounce_delay = 1000,
+    },
+  },
 }
