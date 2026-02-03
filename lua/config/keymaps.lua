94b663108b5eb7532d2f8236add912cf0ef9050c
Jorge Henrique <jorge.henrique@nubank.com.br>
2026-02-03 16:48:07 -0300

    add w parenteses shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("i", "w<Tab>", function()
+  vim.cmd("normal! i() ")
+  vim.cmd("normal! F)")
+  vim.cmd("startinsert")
+end, { noremap = true, silent = true, desc = "Quick Parenteses Opening in insert mode" })
+
+vim.keymap.set("n", "w<Tab>", function()
+  vim.cmd("normal! i() ")
+  vim.cmd("normal! F)")
+  vim.cmd("startinsert")
+end, { noremap = true, silent = true, desc = "Quick Parenteses Opening in normal mode" })
