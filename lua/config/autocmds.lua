bdcfbc9aa6db6321539e5628fd19a7c060a34677
Jorge Henrique <jorge.henrique@nubank.com.br>
2026-02-03 15:10:10 -0300

    config json and base support

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,20 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.filetype.add({
+  extension = {
+    base = "json",
+  },
+})
+
+require("conform").setup({
+  formatters_by_ft = {
+    json = { "no_sort_json_formatter" },
+  },
+  formatters = {
+    no_sort_json_formatter = {
+      extra_args = { "--no-sort-keys" },
+    },
+  },
+})
